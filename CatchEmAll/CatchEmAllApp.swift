563361642d54fd29e14f428d7a1653d955243aaa
CodeLearnerJas <yangtianjin2013@gmail.com>
2024-09-23 13:04:44 +1000

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by GuitarLearnerJas on 23/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchEmAllTests/CatchEmAllTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllTests.swift
+//  CatchEmAllTests
+//
+//  Created by GuitarLearnerJas on 23/9/2024.
+//
+
+import Testing
+@testable import CatchEmAll
+
+struct CatchEmAllTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
